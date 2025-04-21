6db48c2e0f89c49eac0ebb65e08cedd9176adc30
ramonmoreno1 <ramonmoreno1@hotmail.es>
2025-04-21 07:31:46 +0200

    Modifcadp scrip init credenciales mediante .env para seguridad de claves

--- init-db.sh
+++ init-db.sh
@@ -2,11 +2,11 @@
 
 # Script para importar la base de datos si existe un dump
 
-if [ -f "db.sql" ]; then
+if [ -f "./mysql-dump/db.sql" ]; then
   echo "Importando base de datos desde db.sql..."
   docker-compose up -d db
   sleep 10
-  docker exec -i mysql_db mysql -u root -pramon wordpress < db.sql
+  docker exec -i mysql_db mysql -u root -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < ./mysql-dump/db.sql
   echo "Base de datos importada."
 else
   echo "No se encontró db.sql, se usará una base vacía."
